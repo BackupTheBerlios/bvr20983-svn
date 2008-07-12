f6456b62c77391b6476e3214ace42bb94eb2362f
dwachmann <dwachmann@01137330-e44e-0410-aa50-acf51430b3d2>
2008-07-12 19:29:35 +0000

    First stable release with installation support in ie and with cabinet files using INF installation

---
+++ branches/2.0/inc/bvr20983msgs-ver.h
@@ -0,0 +1,34 @@
+/*
+ * $Id$
+ * 
+ * Copyright (C) 2008 Dorothea Wachmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+#if !defined(BVR20983MSGSVER_H)
+#define BVR20983MSGSVER_H
+
+#include "bvr20983-prod.h"
+
+#define verFileVer      1,0,0,76
+#define verFileVer1     "1.0.0.76"
+#define verFileDesc     "Product installation and message component"
+#define verIntName      "msgs"
+#define verOrigName     "bvr20983.2.msgs.1.dll"
+#define verMakeDate     "2008-07-11T15:42:28.426687Z"
+#define verCompPrefix   "msgs.1"
+#define verDebugVersion "1"
+
+#endif // BVR20983MSGSVER_H
+
