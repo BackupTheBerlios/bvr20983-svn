cc4b7117e3986f0941f5b03e9a03634694744507
dwachmann <dwachmann@01137330-e44e-0410-aa50-acf51430b3d2>
2008-07-12 20:25:11 +0000

    BVR20983 Release 2.0.0

---
+++ tags/2.0.0/inc/bvr20983sc-ver.h
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ * 
+ * Copyright (C) 2008 Dorothea Wachmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+#if !defined(BVR20983SCVER_H)
+#define BVR20983SCVER_H
+
+#include "bvr20983-prod.h"
+
+#define verFileVer      1,0,0,79
+#define verFileVer1     "1.0.0.79"
+#define verFileDesc     "Smartcard Library"
+#define verIntName      "sc"
+#define verOrigName     "bvr20983.2.sc.1.dll"
+#define verMakeDate     "2008-07-12T19:30:11.850182Z"
+#define verCompPrefix   "sc.1"
+#define verDebugVersion "1"
+
+#endif // BVR20983SCVER_H
---
+++ tags/2.0.0/inc/lstypeinfo-ver.h
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ * 
+ * Copyright (C) 2008 Dorothea Wachmann
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+#if !defined(LSTYPEINFOVER_H)
+#define LSTYPEINFOVER_H
+
+#include "bvr20983-prod.h"
+
+#define verFileVer      1,0,0,79
+#define verFileVer1     "1.0.0.79"
+#define verFileDesc     "list typeinfo of registered COM objects"
+#define verIntName      "lstypeinfo"
+#define verOrigName     "bvr20983.2.lstypeinfo.1.exe"
+#define verMakeDate     "2008-07-12T19:30:11.850182Z"
+#define verCompPrefix   "lstypeinfo.1"
+#define verDebugVersion "1"
+
+#endif // LSTYPEINFOVER_H
